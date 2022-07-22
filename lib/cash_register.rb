4b2ab54080d0dad8ae35664c846f3f94a83c0736
Kurt Baux <ktripodibaux@gmail.com>
2022-07-22 12:49:22 -0400

    commit tree 658b6e6987d47c54b5c263ccae48bd6335936263

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,28 @@
+class CashRegister
 
+
+
+    attr_accessor :discount, :total
+
+
+    def initialize (value = 0)
+        @total = 0
+        @discount = value
+    end
+
+    def add_item (title, price, quantity = 1)
+        amount = price * quantity
+        @total += amount
+    end
+
+    def apply_discount
+        if @discount != 0
+            @total = @total - (@total * @discount / 100)
+            return "After the discount, the total comes to $#{@total}."
+        else
+            return "There is no discount to apply."
+        end
+            
+    end
+
+end
